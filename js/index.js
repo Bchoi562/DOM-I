9ff0e0d462fafc8a6a1b58d8beb369617e4c85b9
Brian Choi <b.choi562@gmail.com>
2020-10-26 16:01:54 -0700

    Finished module 1 project

--- js/index.js
+++ js/index.js
@@ -38,5 +38,125 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let logo = document.getElementById('logo-img');
+logo.setAttribute('src','/img/logo.png');
+
+const bigLogo = document.getElementById('cta-img');
+bigLogo.setAttribute('src', '/img/header-img.png');
+bigLogo.style.marginLeft = '4rem'
+
+
+const logoh1 = document.querySelector('.cta-text h1');
+logoh1.textContent = 'DOM Is Awesome';
+logoh1.innerText = siteContent.cta.h1.split(' ').join('\n');
+
+
+
+
+const navLinks = document.querySelector('header nav a');
+navLinks.textContent = "Services";
+navLinks.style.color = 'green'
+
+const navLinks2 = navLinks.nextElementSibling;
+navLinks2.textContent = "Product";
+navLinks2.style.color = 'green'
+
+const navLinks3 = navLinks2.nextElementSibling
+navLinks3.textContent = "Vision";
+navLinks3.style.color = 'green';
+
+const navLinks4 = navLinks3.nextElementSibling
+navLinks4.textContent = "Features";
+navLinks4.style.color = 'green';
+
+const navLinks5 = navLinks4.nextElementSibling;
+navLinks5.textContent = "About";
+navLinks5.style.color = 'green';
+
+const navLinks6 = navLinks5.nextElementSibling;
+navLinks6.textContent = "Contact";
+navLinks6.style.color = 'green';
+
+const more = navLinks6.cloneNode(true);
+more.textContent = 'More';
+document.querySelector('header nav').appendChild(more);
+
+const evenmore = more.cloneNode(true);
+evenmore.textContent = "Even More";
+document.querySelector('header nav').appendChild(evenmore);
+
+const ctaButton = document.querySelector('button');
+
+ctaButton.textContent = 'Get Started';
+
+const h4features = document.querySelector('.text-content h4');
+h4features.textContent = "Features";
+
+const featuresText = document.querySelector('.text-content p')
+featuresText.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+// const h4about = h4features.cloneNode(true);
+// document.querySelector('.top-content').appendChild(h4about);
+// h4about.textContent = 'About';
+
+// const aboutText = featuresText.cloneNode(true);
+// aboutText.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const h4about = document.querySelector('.text-content:nth-of-type(2) h4');
+h4about.textContent = "About";
+
+const aboutText = document.querySelector('.text-content:nth-of-type(2) p');
+aboutText.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
+const middleimg = document.querySelector('.middle-img');
+middleimg.src = '/img/mid-page-accent.jpg';
+
+
+
+const h4Services = document.querySelector('.bottom-content .text-content:nth-of-type(1) h4');
+h4Services.textContent = "Services";
+
+const servicesText = document.querySelector('.bottom-content .text-content:nth-of-type(1) p');
+servicesText.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const h4Product = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4');
+h4Product.textContent = "Product";
+
+const productText = document.querySelector('.bottom-content .text-content:nth-of-type(2) p');
+productText.textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
+const h4Vision = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4');
+h4Vision.textContent = "Vision";
+ 
+const visionText = document.querySelector('.bottom-content .text-content:nth-of-type(3) p');
+// visionText.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+visionText.textContent = siteContent['main-content']['vision-content'];
+// ^plugs in from top of page selector.
+
+// const maincontent = siteContent['main-content']
+// alt + shift + down/up copy line to next line
+//alt + down/up move line to next line. 
+
+
+
+
+const contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = "Contact";
+
+const address = document.querySelector('.contact p:nth-of-type(1)');
+address.innerText = "123 Way 456 Street \n Somewhere, USA"
+
+const phone = document.querySelector('.contact p:nth-of-type(2)');
+phone.textContent = "1 (888) 888-8888";
+
+const email = document.querySelector('.contact p:nth-of-type(3)');
+email.textContent = "sales@greatidea.io";
+
+
+const copyright = document.querySelector('footer p');
+copyright.textContent = "Copyright Great Idea! 2018";
